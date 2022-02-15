d98087564262e1904f17fcc7040d13223869beaf
BwanaQ <thunjawax@gmail.com>
2022-02-15 16:16:30 +0300

    add views for registration and sessions

--- buupass/views.py
+++ buupass/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from decimal import Decimal
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import User, Bus, Book
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from .forms import UserLoginForm, UserRegisterForm
+from django.contrib.auth.decorators import login_required
+from decimal import Decimal
+
+
+# Create your views here.
+def signin(request):
+    context = {}
+    if request.method == 'POST':
+        name_r = request.POST.get('name')
+        password_r = request.POST.get('password')
+        user = authenticate(request, username=name_r, password=password_r)
+        if user:
+            login(request, user)
+            # username = request.session['username']
+            context["user"] = name_r
+            context["id"] = request.user.id
+            return render(request, 'myapp/success.html', context)
+            # return HttpResponseRedirect('success')
+        else:
+            context["error"] = "Provide valid credentials"
+            return render(request, 'myapp/signin.html', context)
+    else:
+        context["error"] = "You are not logged in"
+        return render(request, 'myapp/signin.html', context)
+
+
+def signout(request):
+    context = {}
+    logout(request)
+    context['error'] = "You have been logged out"
+    return render(request, 'myapp/signin.html', context)
+
+
+def success(request):
+    context = {}
+    context['user'] = request.user
+    return render(request, 'myapp/success.html', context)
